44e2359d213d9bb4fb94baefc6cf951398fdc155
junghyomoon <dsinhome70@gmail.com>
2021-03-03 14:10:56 +0900

    normal js version

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "junghyo", age = 27, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`
+        Hello! nice you meet you!
+        Is your name ${name}?
+        You are ${age} years old.
+        And you are ${gender}
+    `);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+const name = "junghyo",
+    age = 27, 
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`
+        Hello! nice you meet you!
+        Is your name ${name}?
+        You are ${age} years old.
+        And you are ${gender}
+    `);
+}
+
+sayHi(name, age, gender);
+
+export {};

5f92bb386d3c6b31430dfdeebd1025661265abe0
junghyomoon <dsinhome70@gmail.com>
2021-03-03 14:22:29 +0900

    there are types in ts

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "junghyo", age = 27, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`
+    const greeting = `
         Hello! nice you meet you!
         Is your name ${name}?
         You are ${age} years old.
-        And you are ${gender}
-    `);
+        And you are ${gender}!
+    `;
+    return greeting;
 };
-sayHi(name, age, gender);
+const hello = sayHi(name, age, gender);
+console.log(hello);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,17 +2,18 @@ const name = "junghyo",
     age = 27, 
     gender = "male";
 
-const sayHi = (name, age, gender?) => {
-    console.log(`
+const sayHi = (name:string, age:number, gender:string): string => {
+    const greeting:string = `
         Hello! nice you meet you!
         Is your name ${name}?
         You are ${age} years old.
-        And you are ${gender}
-    `);
+        And you are ${gender}!
+    `;
+    return greeting;
 }
 
-sayHi(name, age, gender);
-
+const hello = sayHi(name, age, gender);
+console.log(hello);
 
 // 이건 왜 필요하죠??
 export {};
